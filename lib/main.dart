f3276d8644ba5cf57cba75701d912ee544632e1b
Haziel Covarrubias <haziel.covarrubias@outlook.com>
2026-02-01 21:47:19 -0700

    feat: Añadir pantalla de contador con funcionalidad para incrementar y decrementar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_curso_bases/screens/counter_screen.dart';
+import 'package:flutter_curso_bases/screens/counter/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
